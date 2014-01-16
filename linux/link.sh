7c57f9fc05bc57a36f29a4ae130cf2c637934dd3
Hiraku NAKANO <hiraku@tojiru.net>
2014-01-17 00:50:56 +0900

    update

--- linux/link.sh
+++ linux/link.sh
@@ -2,6 +2,10 @@
 
 rm -f ~/.bashrc
 rm -f ~/.bash_profile
+rm -f ~/.screenrc
+rm -f ~/.inputrc
+rm -f ~/.vimrc
+rm -rf ~/.vim
 ln -s ~/workspace/dotfiles/.screenrc ~/.screenrc
 ln -s ~/workspace/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/workspace/dotfiles/.inputrc ~/.inputrc
