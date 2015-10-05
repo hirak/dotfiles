1db7dcd0e045a149861e75c3866a15d05a9a14cd
Hiraku NAKANO <hiraku@tojiru.net>
2015-10-06 01:54:55 +0900

    add grep alias

---
+++ .bashrc.d/grepc.sh
@@ -0,0 +1,38 @@
+# for C/C++ header
+function greph() {
+    LANG=C fgrep --color -Inr \
+        --exclude-dir='.git' \
+        --exclude-dir='.svn' \
+        --include='*.h' \
+        --include='*.hpp' \
+        $1 .
+}
+
+# for C/C++
+function grepc() {
+    LANG=C fgrep --color -Inr \
+        --exclude-dir='.git' \
+        --exclude-dir='.svn' \
+        --include='*.h' \
+        --include='*.hpp' \
+        --include='*.m' \
+        --include='*.c' \
+        --include='*.cc' \
+        --include='*.cpp' \
+        --include='*.cxx' \
+        $1 .
+}
+
+# for Web
+function grepw() {
+    LANG=C fgrep --color -Inr \
+      --exclude-dir='.git' \
+      --exclude-dir='.svn' \
+      --include='*.js' \
+      --include='*.css' \
+      --include='*.php' \
+      --include='*.rb' \
+      --include='*.pl' \
+      --include='*.py' \
+      $1 .
+}
