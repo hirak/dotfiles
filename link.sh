407f2a082f6d9825a73bf1649654656526b4c30b
Hiraku NAKANO <hiraku@tojiru.net>
2016-06-21 15:47:11 +0900

    update

--- link.sh
+++ link.sh
@@ -12,7 +12,6 @@ rm -f ~/.inputrc
 rm -f ~/.vimrc
 rm -f ~/.gitignore_global
 rm -rf ~/.vim/dein
-#rm -rf ~/.vim/dein/repos/github.com/Shougo/dein.vim
 rm -f ~/.git-completion.bash
 rm -f ~/.git-prompt.sh
 rm -f ~/.gitconfig
@@ -39,25 +38,7 @@ ln -s $BASEDIR/.git-prompt.sh ~/.git-prompt.sh
 if [ ! -d ~/.vim/dein/repos/github.com/Shougo/dein.vim ]
 then
     git clone {git:/,~/.vim/dein/repos}/github.com/Shougo/dein.vim
-    git clone {git:/,~/.vim/dein/repos}/github.com/NLKNguyen/papercolor-theme
     git clone {git:/,~/.vim/dein/repos}/github.com/Shougo/vimproc
-    git clone {git:/,~/.vim/dein/repos}/github.com/Shougo/neocomplete
-    git clone {git:/,~/.vim/dein/repos}/github.com/Shougo/neosnippet
-    git clone {git:/,~/.vim/dein/repos}/github.com/Shougo/neosnippet-snippets
-    git clone {git:/,~/.vim/dein/repos}/github.com/Shougo/unite.vim
-    git clone {git:/,~/.vim/dein/repos}/github.com/ekalinin/Dockerfile.vim
-    git clone {git:/,~/.vim/dein/repos}/github.com/elzr/vim-json
-    git clone {git:/,~/.vim/dein/repos}/github.com/itchyny/landscape.vim
-    git clone {git:/,~/.vim/dein/repos}/github.com/itchyny/lightline.vim
-    git clone {git:/,~/.vim/dein/repos}/github.com/joonty/vdebug
-    git clone {git:/,~/.vim/dein/repos}/github.com/kchmck/vim-coffee-script
-    git clone {git:/,~/.vim/dein/repos}/github.com/mattn/emmet-vim
-    git clone {git:/,~/.vim/dein/repos}/github.com/mxw/vim-jsx
-    git clone {git:/,~/.vim/dein/repos}/github.com/pangloss/vim-javascript
-    git clone {git:/,~/.vim/dein/repos}/github.com/thinca/vim-localrc
-    git clone {git:/,~/.vim/dein/repos}/github.com/thinca/vim-quickrun
-    git clone {git:/,~/.vim/dein/repos}/github.com/toyamarinyon/vim-swift
-    git clone {git:/,~/.vim/dein/repos}/github.com/tyru/open-browser-github.vim
-    git clone {git:/,~/.vim/dein/repos}/github.com/tyru/open-browser.vim
-    git clone {git:/,~/.vim/dein/repos}/github.com/vim-jp/vim-go-extra
+    cd ~/.vim/dein/repos/github.com/Shougo/vimproc
+    make
 fi
